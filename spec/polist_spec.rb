6e6aef96e34b81b2c74db25d35fed101226f8ac6
Yuri Smirnov <tycooon@yandex.ru>
2017-07-05 18:43:22 +0300

    initial commit

---
+++ spec/polist_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Polist do
+  it "has a version number" do
+    expect(Polist::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
