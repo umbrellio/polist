fa24f0bd376498d71152b8caf1f70115a8613337
Yuri Smirnov <tycooon@yandex.ru>
2017-07-05 20:17:48 +0300

    fix loading issue

--- lib/polist/service.rb
+++ lib/polist/service.rb
@@ -4,85 +4,87 @@ require "active_model/validations"
 require "plissken"
 require "tainbox"
 
-class Polist::Service
-  class Failure < StandardError
-    attr_accessor :response
+module Polist
+  class Service
+    class Failure < StandardError
+      attr_accessor :response
 
-    def initialize(response)
-      self.response = response
-      super
+      def initialize(response)
+        self.response = response
+        super
+      end
     end
-  end
 
-  class Form
-    include Tainbox
-    include ActiveModel::Validations
-  end
-
-  attr_accessor :params
-
-  def self.call(*args)
-    build(*args).tap(&:call)
-  end
-
-  def self.run(*args)
-    build(*args).tap(&:run)
-  end
-
-  def self.param(*names)
-    names.each do |name|
-      define_method(name) { params.fetch(name) }
+    class Form
+      include Tainbox
+      include ActiveModel::Validations
     end
-  end
 
-  def initialize(params)
-    self.params = params
-  end
+    attr_accessor :params
 
-  def call; end # Should be implemented in subclasses
+    def self.call(*args)
+      build(*args).tap(&:call)
+    end
 
-  def run
-    call
-  rescue Failure => error
-    @response = error.response
-    @failure = true
-  end
+    def self.run(*args)
+      build(*args).tap(&:run)
+    end
 
-  def response
-    @response ||= {}
-  end
+    def self.param(*names)
+      names.each do |name|
+        define_method(name) { params.fetch(name) }
+      end
+    end
 
-  def failure?
-    !!@failure
-  end
+    def initialize(params)
+      self.params = params
+    end
 
-  def success?
-    !failure?
-  end
+    def call; end # Should be implemented in subclasses
 
-  def validate!
-    error!(form.errors.to_h.values.first) unless form.valid?
-  end
+    def run
+      call
+    rescue Failure => error
+      @response = error.response
+      @failure = true
+    end
 
-  private
+    def response
+      @response ||= {}
+    end
 
-  def form
-    @form ||= self.class::Form.new(form_attributes.to_snake_keys)
-  end
+    def failure?
+      !!@failure
+    end
 
-  def form_attributes
-    params
-  end
+    def success?
+      !failure?
+    end
 
-  def fail!(response = {})
-    raise self.class::Failure.new(response)
-  end
+    def validate!
+      error!(form.errors.to_h.values.first) unless form.valid?
+    end
 
-  def error!(message = "")
-    fail!(error: message)
-  end
+    private
 
-  def success!(response = {})
-    @response = response
+    def form
+      @form ||= self.class::Form.new(form_attributes.to_snake_keys)
+    end
+
+    def form_attributes
+      params
+    end
+
+    def fail!(response = {})
+      raise self.class::Failure.new(response)
+    end
+
+    def error!(message = "")
+      fail!(error: message)
+    end
+
+    def success!(response = {})
+      @response = response
+    end
   end
 end
