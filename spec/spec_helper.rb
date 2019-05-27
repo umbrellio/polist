61593cb3899a9ee3731ea51a1d0dafb1894a06cc
Rustam Ibragimov <iamdaiver@gmail.com>
2019-05-27 23:53:17 +0300

    randomized specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "bundler/setup"
 require "simplecov"
 require "coveralls"
 
@@ -12,16 +11,13 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.minimum_coverage(100)
 SimpleCov.start
 
+require "bundler/setup"
 require "polist"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
+  config.order = :random
+  Kernel.srand config.seed
   config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
 end
