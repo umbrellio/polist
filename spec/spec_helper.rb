e49294b251560be9555885056e524b5a87d4d98e
Yuri Smirnov <tycooon@yandex.ru>
2021-06-18 19:52:09 +0300

    return random spec order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,9 @@ SimpleCov.start
 require "polist"
 
 RSpec.configure do |config|
+  config.order = :random
+  Kernel.srand config.seed
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
